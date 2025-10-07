5521bf4e12f4478004812d3ecbbd404e6413bfdc
Malyun <maa026@edu.zealand.dk>
2025-10-07 10:59:10 +0200

    task 1 done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+// Funktion til at oprette et nyt <li>-element
+function lavLi(klasse, id, tekst) {
+  const li = document.createElement("li");              // Opretter <li>
+  li.setAttribute("class", klasse);                     // Tilføjer class
+  li.setAttribute("id", id);                            // Tilføjer id
+  const tekstNode = document.createTextNode(tekst);     // Opretter tekst-node
+  li.appendChild(tekstNode);                            // Tilføjer tekst til <li>
+  return li;                                            // Returnerer <li>
+}
+
+// Tilføjer et nyt element til listen
+const liste = document.getElementById("listUnhealthy");
+const nyUgesynd = lavLi("unhealthy", "5", "Chips");
+liste.appendChild(nyUgesynd);
